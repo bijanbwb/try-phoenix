d3d4f3224d5c10018de3d70365529cc2ac5889d0
Bijan <bijanbwb@gmail.com>
2016-05-20 18:22:17 -0400

    Write test for dynamic name feature

--- test/controllers/hello_controller_test.exs
+++ test/controllers/hello_controller_test.exs
@@ -5,4 +5,9 @@ defmodule HelloPhoenix.HelloControllerTest do
     conn = get conn, "/hello"
     assert html_response(conn, 200) =~ "Hello World, from Phoenix!"
   end
+
+  test "GET /hello/Bijan", %{conn: conn} do
+    conn = get conn, "/hello/Bijan"
+    assert html_response(conn, 200) =~ "Hello World, from Bijan!"
+  end
 end
